0c5d6ed0b6e31a60d3d528055a4fc6c669b9aeec
Rachel Ho <rachelhoyuenyi@gmail.com>
2020-09-14 21:56:25 +0800

    color picker for stroke color

--- javascript/drawing-circle.js
+++ javascript/drawing-circle.js
@@ -1,44 +1,51 @@
 class DrawingCircle extends PaintFunction {
-    constructor(contextReal, contextDraft) {
-        super();
-        this.contextReal = contextReal;
-        this.contextDraft = contextDraft;
-    }
+  constructor(contextReal, contextDraft) {
+    super();
+    this.contextReal = contextReal;
+    this.contextDraft = contextDraft;
+  }
 
-    onMouseDown(coord, e) {
-        this.contextReal.fillStyle = "#f44";
-        this.origX = coord[0];
-        this.origY = coord[1];
+  onMouseDown(coord, e) {
+    this.contextReal.fillStyle = "red";
+    this.origX = coord[0];
+    this.origY = coord[1];
+  }
 
-    }
+  onDragging(coord, e) {
+    this.contextDraft.fillStyle = "red";
+    var radius = 0;
+    var deltaX = this.origX - coord[0];
+    var deltaY = this.origY - coord[1];
+    radius = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    this.contextDraft.clearRect(
+      0,
+      0,
+      canvasDraft.clientWidth,
+      canvasDraft.height
+    );
+    this.contextDraft.beginPath();
+    this.contextDraft.arc(this.origX, this.origY, radius, 0, Math.PI * 2);
+    this.contextDraft.fill();
+  }
 
-    onDragging(coord, e) {
-        this.contextDraft.fillStyle = "#f44";
-        var radius = 0;
-        var deltaX = this.origX - coord[0];
-        var deltaY = this.origY - coord[1];
-        radius = Math.sqrt(deltaX * deltaX + deltaY * deltaY)
-        this.contextDraft.clearRect(0, 0, canvasDraft.clientWidth, canvasDraft.height);
-        this.contextDraft.beginPath();
-        this.contextDraft.arc(this.origX, this.origY, radius, 0, Math.PI*2);
-        this.contextDraft.fill();
+  onMouseMove() {}
 
-    }
+  onMouseUp(coord) {
+    var radius = 0;
+    var deltaX = this.origX - coord[0];
+    var deltaY = this.origY - coord[1];
+    radius = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    this.contextDraft.clearRect(
+      0,
+      0,
+      canvasDraft.clientWidth,
+      canvasDraft.height
+    );
+    this.contextReal.beginPath();
+    this.contextReal.arc(this.origX, this.origY, radius, 0, Math.PI * 2);
+    this.contextReal.fill();
+  }
 
-    onMouseMove(){}
-
-    onMouseUp(coord) {
-        var radius = 0;
-        var deltaX = this.origX - coord[0];
-        var deltaY = this.origY - coord[1];
-        radius = Math.sqrt(deltaX * deltaX + deltaY * deltaY)
-        this.contextDraft.clearRect(0, 0, canvasDraft.clientWidth, canvasDraft.height);
-        this.contextReal.beginPath();
-        this.contextReal.arc(this.origX, this.origY, radius, 0, Math.PI*2);
-        this.contextReal.fill();
-    }
-
-    onMouseLeave() {}
-    onMouseEnter() {}   
-
+  onMouseLeave() {}
+  onMouseEnter() {}
 }
---
+++ javascript/variables.js
@@ -0,0 +1,1 @@
+let curStroke = "red";
