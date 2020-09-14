c269a1ed334bb9107a0ec788d4bb20d028ae0927
Mika Fung <fungcheukman@hotmail.com>
2020-09-14 23:15:40 +0800

    CURVE DONE

--- javascript/drawing-quadratic-curve.js
+++ javascript/drawing-quadratic-curve.js
@@ -3,53 +3,62 @@ class DrawingQuadraticCurve extends PaintFunction {
         super();
         this.contextReal = contextReal;
         this.contextDraft = contextDraft;
-        this.click = 0;
+        this.variable = false;
     }
 
     onMouseDown(coord, event) {
-        if (this.click === 0) {
-            this.contextReal.strokeStyle = '#000';
-            this.contextReal.lineWidth = 5;
-            this.contextReal.beginPath();
+        // 1. first click to get start point
+        if (this.variable === false) {
             this.origX = coord[0];
             this.origY = coord[1];
-        } else if (this.click === 1) {
-            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-            this.contextReal.moveTo(this.origX, this.origY);
-            this.contextReal.quadraticCurveTo(coord[0], coord[1], this.newX, this.newY);
-            this.contextReal.stroke();
-            console.log('final' + coord[0] + ' ' + coord[1]);
         }
-        
     }
 
     onDragging(coord, event) {
-        if (this.click === 0) {
-            this.draw(coord[0], coord[1]);
-        }
-    }
-    onMouseMove(coord, event) {
-        console.log(coord[0] + ' ' + coord[1]);
-    }
-    onMouseUp(coord, event) {
-        if (this.click === 0) {
-            this.newX = coord[0];
-            this.newY = coord[1];
-            this.contextDraft.moveTo(this.newX, this.newY);
+        // 2. after first click, drag to end point of real curve
+        if (this.variable === false) {
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.contextDraft.beginPath();
+            this.contextDraft.moveTo(this.origX, this.origY);
             this.contextDraft.lineTo(coord[0], coord[1]);
             this.contextDraft.stroke();
-            this.click++;
+        } else if (this.variable === true) {
+            // 5. second drag to controlpoint of real curve
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.contextDraft.beginPath();
+            this.contextDraft.moveTo(this.origX, this.origY);
+            this.contextDraft.quadraticCurveTo(coord[0], coord[1], this.endX, this.endY);
+            this.contextDraft.stroke();
         }
     }
-    // onMouseLeave(){}
-    // onMouseEnter(){}
-
-    draw(x, y) {
-        this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-        this.contextDraft.beginPath();
-        this.contextDraft.moveTo(this.origX, this.origY);
-        this.contextDraft.lineTo(x, y);
-        this.contextDraft.stroke();
+    onMouseMove(coord, event) {}
+    onMouseUp(coord, event) {
+        if (this.variable === false) {
+            // 3. after first dragging, mouseup to get the end point of real curve
+            this.endX = coord[0];
+            this.endY = coord[1];
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.contextDraft.beginPath();
+            this.contextDraft.moveTo(this.origX, this.origY);
+            this.contextDraft.lineTo(this.endX, this.endY);
+            this.contextDraft.stroke();
+            // 4. initiate second dragging to find the control point
+            this.variable = true;
+        } else if (this.variable === true) {
+            // 6. draw the real curve
+            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            this.contextReal.strokeStyle = '#000';
+            this.contextReal.lineWidth = 5;
+            this.contextReal.beginPath();
+            this.contextReal.moveTo(this.origX, this.origY);
+            this.contextReal.quadraticCurveTo(coord[0], coord[1], this.endX, this.endY);
+            this.contextReal.stroke();
+            this.variable = false;
+        }
     }
+    onMouseLeave(){}
+    onMouseEnter(){}
+
+    
 
 }
