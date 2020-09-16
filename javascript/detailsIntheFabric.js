0516444c6f202f35d034a4445e34b583bc107851
Mika Fung <fungcheukman@hotmail.com>
2020-09-16 18:19:10 +0800

    added stamp function

--- javascript/detailsIntheFabric.js
+++ javascript/detailsIntheFabric.js
@@ -18,36 +18,38 @@ function AddRect() {
   canvas.add(rect);
 }
 
-function AddTrangle() {
-  var trangle = new fabric.Triangle({
+function AddTriangle() {
+  var triangle = new fabric.Triangle({
     left: 70,
     top: 50,
-    fill: "yellow",
+    fill: curFillColor,
     width: 90,
     height: 100,
-    stroke: "lime",
+    stroke: curStroke,
     strokeWidth: 4,
   });
-  canvas.add(trangle);
+  canvas.add(triangle);
 }
 function AddCircle() {
   var circle = new fabric.Circle({
     radius: 50,
-    fill: "green",
+    fill: curFillColor,
     left: 30,
     top: 50,
+    stroke: curStroke,
+    strokeWidth: 4,
   });
   canvas.add(circle);
   canvas.moveTo(circle, 10000000);
 }
+
 function AddMagi() {
-  fabric.Image.fromURL("../assets/imgpng/Magikoopa.png", function (oImg) {
-    oImg.filters.push(new fabric.Image.filters.Grayscale());
-    oImg.applyFilters();
+  fabric.Image.fromURL("./assets/images/Magikoopa.png", function (oImg) {
     canvas.add(oImg);
   });
 }
-var path = new fabric.Path(
+function AddArrow() {
+  var path = new fabric.Path(
   "M121.32,0L44.58,0C36.67,0,29.5,3.22,24.31,8.41\
 c-5.19,5.19-8.41,12.37-8.41,20.28c0,15.82,12.87,28.69,28.69,28.69c0,0,4.4,\
 0,7.48,0C36.66,72.78,8.4,101.04,8.4,101.04C2.98,106.45,0,113.66,0,121.32\
@@ -56,14 +58,17 @@ c0,7.66,2.98,14.87,8.4,20.29l0,0c5.42,5.42,12.62,8.4,20.28,8.4c7.66,0,14.87\
 12.87,28.69,28.69,28.69c7.66,0,14.87-2.99,20.29-8.4c5.42-5.42,8.4-12.62,8.4\
 -20.28l0-76.74c0-7.66-2.98-14.87-8.4-20.29C136.19,2.98,128.98,0,121.32,0z"
 );
+  canvas.add(path.set({ left: 100, top: 200 , fill: curFillColor, stroke: curStroke}));
+}
 
-// canvas.add(path.set({ left: 100, top: 200 }));
+
+
 
 $("#AddRect").click(function () {
   AddRect();
 });
-$("#AddTrangle").click(function () {
-  AddTrangle();
+$("#AddTriangle").click(function () {
+  AddTriangle();
 });
 $("#AddCircle").click(function () {
   AddCircle();
@@ -71,6 +76,10 @@ $("#AddCircle").click(function () {
 $("#AddMagi").click(function () {
   AddMagi();
 });
+$("#AddArrow").click(function () {
+  AddArrow();
+});
+
 
 //   rect.set({ fill: 'red', strokeWidth: 5, stroke: 'rgba(100,200,200,0.5)' });
 //   canvas.add(circle);
